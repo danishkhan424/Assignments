ff6e243eca58549779210ccb039be5689c46b9e2
Danish Khan <danishk0687@gmail.com>
2024-01-27 21:52:17 +0530

    added my-counter-app

---
+++ React/React-4/State-Management-Vite/Counter-using-Vite/my-counter-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import Counter from './Counter.jsx';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Counter count={count} setCount={setCount} />
+    </div>
+  );
+}
+
+export default App;
