8a7558980774f0e2d428900b3b4272f062168029
Danish Khan <danishk0687@gmail.com>
2024-07-25 22:43:26 +0530

    changed file

--- Backend/Level-2-HTTP-Basics-Express/Node_File_Server_Dynamic/index.js
+++ Backend/Level-2-HTTP-Basics-Express/Node_File_Server_Dynamic/index.js
@@ -1,34 +1,3 @@
-/*
-Topics Covered:
-        
-    HTTP Protocol
-    Node.js http and path Modules
-    Dynamic Content Generation
-    File System Operations
-
-
-Your task is to develop a dynamic file server that:
-
-Provides a directory listing on the root URL (/), displaying all files and folders with appropriate icons (folders: , files: ).
-Enables navigation into nested directories through URL paths, dynamically generating listings for each directory visited.
-Serves actual file contents when a file is accessed directly.
-Returns a "404 Not Found" error for requests to non-existent directories or files.
-Utilizes the Node.js path module to correctly navigate filesystem paths based on URL routes.
-Hints:
-
-For the UI, manipulate <li> tags to include the Unicode HTML-code for folders and files, enhancing the visual presentation.
-Dynamically generate HTML to display directory contents with proper icons and anchor tags for navigation.
-Examples:
-Accessing / displays a list of all directories and files in the root, with icons indicating type (directory or file):
-!https://i.imgur.com/8Msqzs7.png
-
-Accessing /__tests__ shows the contents of the __tests__ directory with appropriate icons:
-!https://i.imgur.com/l2s6vlu.png
-
-Navigating to /__tests__/other dynamically generates a listing for the other directory inside __tests__:
-!https://i.imgur.com/LZki7ht.png
-*/
-
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
