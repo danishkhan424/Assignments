fa500c1050bbf00a83dfb3fe6d0db619a0359b47
danish khan <danishk0687@gmail.com>
2025-04-22 23:55:31 +0530

    added  problem's solution

--- FEDC-2504/Functions-and-Object-Methods/Problem-15/script.js
+++ FEDC-2504/Functions-and-Object-Methods/Problem-15/script.js
@@ -2,10 +2,19 @@ let sentenceBuilder ={
   subject: "I",
   verb: "am",
   object: "coding",
-  buildSentence : () => {
+  buildSentence : function () {
+    let sentence = (this.subject === "" || this.verb === "" || this.object === "") ? "Incomplete Sentence" :  this.subject + " " + this.verb + " " + this.object ;
+    console.log(sentence);
     
   },
-  updateProperty : (property, value) => {
-
+  updateProperty : function (property, value) {
+    this.hasOwnProperty(property) ? this[property] = value : console.log("Invalid property");
   }
 }
+
+sentenceBuilder.buildSentence()
+sentenceBuilder.updateProperty("verb", "am learning");
+sentenceBuilder.updateProperty("verb", "is learning");
+sentenceBuilder.updateProperty("subject", "The cat");
+sentenceBuilder.buildSentence();
+sentenceBuilder.updateProperty("mood", "happy");
